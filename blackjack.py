691af3daef726f886c3e10152ccaffe914d0fb7d
JKBODY <77995137+JKB0DY@users.noreply.github.com>
2021-07-04 14:30:25 +0200

    completed Translation
    I forgot to translate this part into python 3

--- blackjack.py
+++ blackjack.py
@@ -25,6 +25,50 @@ pygame.mixer.init()
 
 screen = pygame.display.set_mode((800, 480))
 clock = pygame.time.Clock()
+
+
+###### SYSTEM FUNCTIONS BEGIN #######
+def imageLoad(name, card):
+    """ Function for loading an image. Makes sure the game is compatible across multiple OS'es, as it
+    uses the os.path.join function to get he full filename. It then tries to load the image,
+    and raises an exception if it can't, so the user will know specifically what's going if the image loading
+    does not work. """
+    
+    if card == 1:
+        fullname = os.path.join("images/cards/", name)
+    else: fullname = os.path.join('images', name)
+    
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:' + name)
+        raise SystemExit(message)
+        
+    image = image.convert()
+    
+    return image, image.get_rect()
+        
+def soundLoad(name):
+    """ Same idea as the imageLoad function. """
+    
+    fullName = os.path.join('sounds', name)
+    try: sound = pygame.mixer.Sound(fullName)
+    except pygame.error as message:
+        print('Cannot load sound: ' + name)
+        raise SystemExit(message)
+    return sound
+
+def display(font, sentence):
+    """ Displays text at the bottom of the screen, informing the player of what is going on."""
+    
+    displayFont = pygame.font.Font.render(font, sentence, 1, (255,255,255), (0,0,0)) 
+    return displayFont
+
+def playClick():
+    clickSound = soundLoad("click2.wav")
+    clickSound.play()
+###### SYSTEM FUNCTIONS END #######
+
 ###### MAIN GAME FUNCTION BEGINS ######
 def mainGame():
     """ Function that contains all the game logic. """
